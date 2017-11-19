1a288fdd2e3fe50096c57ad772e0dd244fe9803d
smunro16 <smunr16@gmai.com>
2017-11-19 20:54:28 +0000

    Post and get from mongo

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
 var express = require('express'),
-    mongoose = require('mongoose');
+    mongoose = require('mongoose'),
+    bodyParser = require('body-parser');
 
-var db=mongoose.connect('mongodb://smunro16:<password>@cluster0-shard-00-00-gzjjw.mongodb.net:27017,cluster0-shard-00-01-gzjjw.mongodb.net:27017,cluster0-shard-00-02-gzjjw.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin');
+var db=mongoose.connect('mongodb://smunro16:<pwd>@cluster0-shard-00-00-gzjjw.mongodb.net:27017,cluster0-shard-00-01-gzjjw.mongodb.net:27017,cluster0-shard-00-02-gzjjw.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin');
 
-var book=require('./models/bookModels.js');
+var Book=require('./models/bookModels.js');
 
 var app = express();
 var port = process.env.PORT||3000;
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
 
 var bookRouter = express.Router();
 
 
 bookRouter.route('/Books')
+    .post(function(req, res){
+        var book = new Book(req.body);
+        book.save();
+        res.status(201).send(book);
+    })
     .get(function(req, res){
 
         var query = {};
@@ -26,7 +35,18 @@ bookRouter.route('/Books')
             else
                 res.json(books);
         });
-        res.json(responseJson);
+        // res.json(responseJson);
+    });
+
+bookRouter.route('/Books/:bookId')
+    .get(function(req, res){
+        Book.findById(req.params.bookId, function(err, book){
+            if(err)
+                res.status(500).send(err);
+            else
+                res.json(book);
+        });
+        // res.json(responseJson);
     });
 
 app.use('/api', bookRouter);
--- models/bookModels.js
+++ models/bookModels.js
@@ -1,19 +1,13 @@
-var mongoose=require('mongoose'),
-    Schema=mongoose.Schema;
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
 
-var bookModel=new Schema({
-    title:{
+var bookModel = new Schema({
+    title: {
         type: String
     },
-    author:{
-        type: String
-    },
-    genre:{
-        type: String
-    },
-    read:{
-        type: Boolean, default:false
-    }
+    author: {type: String},
+    genre: {type: String},
+    read: {type: Boolean, default:false}
 });
 
-module.export=mongoose.model('Book', bookModel);
+module.exports= mongoose.model('Book', bookModel);
